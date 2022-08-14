e5e195a4a840ea25c403ae4b3576f00be7fb367b
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-14 19:37:45 +0530

    imported extion and StyledComponents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import styled from "styled-components";
+import Section from "./Section";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
