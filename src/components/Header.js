437cd56c96626e463491ee79e0c78e5e8c3b54f0
HemanthKumar <m.hemanthkmr143@gmail.com>
2022-08-17 21:11:56 +0530

    Deveoloped a Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,67 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Container>
+      <a>
+        <img src="images/logo.svg" alt="" />
+      </a>
+
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+        <a href="#">Solar Roof</a>
+        <a href="#">Solar Panels</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <a href="#">Menu</a>
+      </RightMenu>
+    </Container>
   );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0px 10px;
+    flex-wrap: nowrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
